671d43c87c94ba0ff37be008f38153ea20179e7e
kvanlaan <kdvanlaan@gmail.com>
2018-03-09 15:17:11 -0600

    added constraints

--- LogicCircuitsConform/src/logiccircuits/Circuit.java
+++ LogicCircuitsConform/src/logiccircuits/Circuit.java
@@ -1,8 +1,11 @@
 package logiccircuits;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 import PrologDB.*;
+import java.util.HashSet;
+
 public class Circuit {
 
     String name;
@@ -16,7 +19,7 @@ public class Circuit {
         gates = new LinkedList<>();
         wires = new LinkedList<>();
     }
-    
+
     public int getGateNum(String type) {
         switch (type) {
             case "and":
@@ -33,7 +36,7 @@ public class Circuit {
                 return -1;
         }
     }
-    
+
     public void addOneGate(String type) {
         switch (type) {
             case "and":
@@ -53,10 +56,10 @@ public class Circuit {
                 break;
         }
     }
-            
+
     public void print() {
         System.out.println("printing circuit tables...");
-        
+
         System.out.println("Gates:");
         gates.stream().forEach(gate -> {
             if (!gate.type.equals("input") && !gate.type.equals("output")) {
@@ -68,21 +71,86 @@ public class Circuit {
                 gate.print();
             }
         });
-        
+
         System.out.println("\nWires:");
         wires.stream().forEach(wire -> {
             wire.print();
         });
     }
-    public void validate() {
-        // call logicConform logic here
 
+    public void validate() {
         ErrorReport er = new ErrorReport();
-//        gates.stream().filter().forEach(;
-        
+
+        // setting up
+        ArrayList<Gate> gatesArray = new ArrayList<Gate>();
+        gates.stream().forEach(gate -> gatesArray.add(gate));
+
+        // constraint 1: all gates have unique names
+        ArrayList<String> gateNames = new ArrayList<String>();
+        gates.stream().forEach(gate -> gateNames.add(gate.getGateId()));
+
+        HashSet<String> gateNameSet = new HashSet<String>();
+        for (String name : gateNames) {
+            if (!gateNameSet.add(name)) {
+                // rule is that gates must have unique id
+                er.add("Duplicate Gate Id is Found");
+            }
+        }
+
+        // Constraint 2: > Circuit must have a list of defined gates (at least length one)
+        if (gates.size() < 1) {
+            er.add("Circuit is missing gates");
+        }
+        // Constraint 3: Circuit must have a list of defined wires (at least length one)
+        if (wires.size() < 1) {
+            er.add("Circuit is missing wires");
+        }
+
+        // Constraint 4: verify that all wires are properly connected
+        // wire connects an output pin to an input pin.
+        ArrayList<Wire> wireArray = new ArrayList<Wire>();
+        wires.stream().forEach(wire -> wireArray.add(wire));
+
+        wires.stream().forEach(wire -> {
+            if (wire.to == null || wire.from == null) {
+                er.add("Wire is missing either a to or from pin");
+            }
+            
+             if (wire.to.equals(wire.from)) {
+                er.add("Wire connects to same gates");
+            }
+                if (wire.from.type.equals("output")) {
+                    er.add("Wire's from gate is an ouput");
+                }
+                if (wire.to.type.equals("input")) {
+                    er.add("Wire's to gate is an input");
+                }
+        });
+        // Constraint 5: Circuit has >1 input pins
+        ArrayList<Gate> inputGates = new ArrayList<Gate>();
+
+        gates.stream().forEach(gate -> {
+            if ((gate.type.equals("input"))) {
+                inputGates.add(gate);
+            }
+        });
+        if (inputGates.size() < 1) {
+            er.add("Circuit is missing an input gate");
+        }
+
+        // Constraint 6:   Circuit has >1 output pins
+        ArrayList<Gate> outputGates = new ArrayList<Gate>();
+        gates.stream().forEach(gate -> {
+            if ((gate.type.equals("output"))) {
+                outputGates.add(gate);
+            }
+        });
+        if (outputGates.size() < 1) {
+            er.add("Circuit is missing an output gate");
+        }
         // Step 4: finish by reporting collected errors
         er.printReportEH(System.out);
-        
+
         if (!er.printReport(System.out)) {
             System.out.format("All constraints satisfied for this cicuit");
         }
